f34e4304402518b23d8f38a9f4e0cff42d054587
syyjbhjh3 <syyjbhjh7@gmail.com>
2024-09-18 22:07:07 +0900

    Update : test코드 작성

--- src/test/java/com/example/follow/DemoApplicationTests.java
+++ src/test/java/com/example/follow/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.follow;
 
+import com.example.follow.service.FollowerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.http.HttpClient;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
+		FollowerService svc = new FollowerService(HttpClient.newHttpClient());
+		try {
+			svc.followingUser("syyjbhjh3");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 
+	}
 }
